fa8a2a1d15f92ec9197b2cfb898be2cfe403d7d4
915marco <915marco@gmail.com>
2022-08-04 19:50:40 -0500

    updated page plan and purpose

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# music-keys
+<!-- # music-keys
 A web application that will allow you to create music with pre-set beats I have created.
 I also wish to enable a way to record the songs you create.
-The application will consists of HTML, CSS, Javascript, NPM, mongoose, and a few other packages from NPM. 
+The application will consists of HTML, CSS, Javascript, NPM, mongoose, and a few other packages from NPM.  -->
+
+A page dedicated to learning html css & js 
--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 console.log('Howdy Marcus!');
+
+
+const myBtn = document.getElementById('myBtn');
+
+// console.log(myBtn);
+
+myBtn.addEventListener("click", () => myBtn.style.backgroundColor = 'rgb(77, 139, 197)' );
+
+    
+    // const name = prompt('What is your name ?');
+    // document.body.innerHTML = ('<h1> ' + name + '</h1>') ;
+
+    const record = document.getElementById('rcdBtn');
+    record.addEventListener("click", () => record.style.backgroundColor = 'green');
+
+    const play = document.getElementById('plyBtn');
+    play.addEventListener("click", () => play.style.backgroundColor = 'blue');
+
+    const erase = document.getElementById('delBtn');
+    erase.addEventListener("click", () => erase.style.backgroundColor = 'black');
+
+    // const square = document.getElementsById('square');
+    // square.addEventListener("click", () => square.style.backgroundColor = 'rgb(77, 139, 197)')
+
+console.log('Howdy');
+
+let person = {
+    firstName : "Marco",
+    lastName : "Moya",
+    age : 25,
+
+    address: {
+        state: 'Texas',
+        apt: 701,
+        zip : 78741,
+    }
+
+};
+ 
+console.log(person.address.zip);
+console.log(person.age)
+
