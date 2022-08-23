ee45e6923593ca3275500794de42b7c4ef374562
915marco <915marco@gmail.com>
2022-08-23 18:50:09 -0500

    buttons to pick a team

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Howdy Partner!")
+
+const cowboyBtn = document.getElementById("howdy");
+cowboyBtn.addEventListener("click", () => cowboyBtn.style.backgroundColor = 'brown');
+
+const alienBtn = document.getElementById("alien");
+alienBtn.addEventListener("click", () => alienBtn.style.backgroundColor = 'teal');
