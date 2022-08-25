f7d497d017d10f478b2701532c58737f635be162
915marco <915marco@gmail.com>
2022-08-24 19:22:00 -0500

    editing first page html

--- script.js
+++ script.js
@@ -3,5 +3,18 @@ console.log("Howdy Partner!")
 const cowboyBtn = document.getElementById("howdy");
 cowboyBtn.addEventListener("click", () => cowboyBtn.style.backgroundColor = 'brown');
 
+
+
 const alienBtn = document.getElementById("alien");
 alienBtn.addEventListener("click", () => alienBtn.style.backgroundColor = 'teal');
+
+// going to add a if else condition for the teams 
+
+if (cowboyBtn = "click") 
+    console.log("Howdy Partner");
+
+    else 
+    console.log("asklcnjbfsk");
+// else if (alienBtn = "click") {
+//     alert("hjsbkbfk z,jsbc,jsb");
+// };
